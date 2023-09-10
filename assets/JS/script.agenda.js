8e0812b673da32e6475add10bc9810d3ed18a535
maria eduarda <123502092+Mariavalonga@users.noreply.github.com>
2023-09-10 16:22:02 -0300

    feat: criai a classe com o nome Agenda

---
+++ assets/JS/script.agenda.js
@@ -0,0 +1,19 @@
+console.log("O Js está linkado!");
+
+class Agenda {
+    constructor(nome,telfixo,celular,foto,niver,email,cep,cidade,insta,git) {
+      this.nome = nome;
+      this.telfixo = telfixo;
+      this.celular = celular;
+      this.foto = foto;
+      this.niver = niver;
+      this.idade = this.verificarIdade(niver);
+      this.signo = this.getZodiacSign();
+      this.id = this.pegarUsuarioId();
+      this.email = email;
+      this.cep = cep;
+      this.cidade = cidade;
+      this.insta = insta;
+      this.git = git;
+    }
+}
